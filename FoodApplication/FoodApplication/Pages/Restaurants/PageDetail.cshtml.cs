35ff16f703d6942c8525e1f3053dc90f2c11996b
KushalSeth <kushalseth1408@gmail.com>
2020-06-03 00:26:17 +0530

    added edit functionality

---
+++ FoodApplication/FoodApplication/Pages/Restaurants/PageDetail.cshtml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodApplication.Core;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Configuration;
+
+namespace FoodApplication.Pages.Restaurants
+{
+    public class PageDetailModel : PageModel
+    {
+        private readonly IConfiguration config;
+
+        public PageDetailModel(IConfiguration config)
+        {
+            this.config = config;
+            Restaurant = new Restaurant();
+        }
+
+        public Restaurant Restaurant { get; set; }
+        public string Message { get; set; }
+
+        // strict input model, so we are not making a Public property
+        public void OnGet(int restaurantId)
+        {
+            Restaurant.Id = restaurantId;
+            Message = config["Message"];
+        }
+    }
+}
