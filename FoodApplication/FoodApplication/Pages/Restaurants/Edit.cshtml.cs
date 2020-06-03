f5bf91b516d3387bb038bc306d1997b259069bab
KushalSeth <kushalseth1408@gmail.com>
2020-06-04 00:39:24 +0530

    edit button

---
+++ FoodApplication/FoodApplication/Pages/Restaurants/Edit.cshtml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using FoodApplication.Core;
+using FoodApplication.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace FoodApplication.Pages.Restaurants
+{
+    public class EditModel : PageModel
+    {
+        private readonly IRestaurantData restaurantData;
+        public Restaurant MyProperty { get; set; }
+
+        public EditModel(IRestaurantData restaurantData)
+        {
+            this.restaurantData = restaurantData;
+        }
+
+        public void OnGet(int restaurantId)
+        {
+
+        }
+    }
+}
